96c44c3d083157bed2e37e989cd346ec7dbf574d
João Filipi <jf.britto@hotmail.com>
2022-12-07 00:05:52 -0300

    Subindo projeto

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useContext, useEffect } from "react";
+import "./App.css";
+import GameOver from "./components/GameOver";
+import Question from "./components/Question";
+import Welcome from "./components/Welcome";
+import { QuizContext } from "./context/quiz";
+
+function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
+
+  useEffect(() => {
+    dispatch({ type: "REORDER_QUESTIONS" });
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Quiz de Programação</h1>
+      {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Playing" && <Question />}
+      {quizState.gameStage === "End" && <GameOver />}
+    </div>
+  );
+}
+
+export default App;
